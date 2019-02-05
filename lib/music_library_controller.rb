a43c1a2a0239468cc0d533fa6978134a5f5362e5
Tom Holmes <tholmes59@gmail.com>
2019-02-05 19:00:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,8 +2,8 @@ class MusicLibraryController
   
   attr_reader :path
   
-  def initialize(path = "./db/mp3s/")
-    @path = path
+  def initialize(path)
+    @path = "./db/mp3s/"
     self.import
   end
   
