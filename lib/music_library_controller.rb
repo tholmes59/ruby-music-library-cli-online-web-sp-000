fc51a7050e35a421db2081e13047f5ace999cf1a
Tom Holmes <tholmes59@gmail.com>
2019-02-05 20:40:41 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -35,4 +35,10 @@ class MusicLibraryController
     end
   end 
   
+  def list_genres
+    Genre.all.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |value, index|
+      puts "#{index}. #{value.name}"
+    end
+  end 
+  
 end
