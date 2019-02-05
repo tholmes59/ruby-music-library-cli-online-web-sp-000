00c4ec1c2fa595562eba674f395d85f4e6936c33
Tom Holmes <tholmes59@gmail.com>
2019-02-05 20:15:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -25,7 +25,7 @@ class MusicLibraryController
   
   def list_songs
     Song.all.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |value, index|
-      "#{index}. #{value.artist.name} - #{value.name} - #{value.genre.name}"
+      puts "#{index}. #{value.artist.name} - #{value.name} - #{value.genre.name}"
     end
   end 
   
