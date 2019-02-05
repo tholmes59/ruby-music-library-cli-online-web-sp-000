2875dcf38c6a126b0201f5a8435d68065397dbc8
Tom Holmes <tholmes59@gmail.com>
2019-02-05 19:18:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,6 +16,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    
+    input = gets.strip
   end 
   
 end
